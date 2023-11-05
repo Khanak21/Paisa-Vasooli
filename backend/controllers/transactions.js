50493c0af43d4cfaf848a66d1777181a599d11aa
Khanak21 <khanakpatwari21@gmail.com>
2023-11-05 18:12:43 +0530

    added transaction controllers

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -26,13 +26,15 @@ const addTransaction = async(req,res)=>{
 
 }
 
-// const getTransactions=async(req,res)=>{
-//     const id= req.params.id;
-//     try{
-//         const trans = await 
-//     }
+const getTransactions=async(req,res)=>{
+    const {userId}= req.body;
+    try{
+        const trans = await transactionSchema.find({userId:userId})
+    }catch(err){
+        res.json({message:"No transactions found"})
+    }
 
-// }
+}
 
 const editTransaction=async(req,res)=>{
 
@@ -43,22 +45,52 @@ const deleteTransaction=async(req,res)=>{
 }
 
 const getTransactionsByCategory=async(req,res)=>{
-
+    const {id,category}= req.body;
+    try{
+        const trans = await transactionSchema.find({userId:id,category:category})
+    }catch(err){
+        res.json({message:"No transactions by given category found"})
+    }
 }
 
 const getTransactionsByDate=async(req,res)=>{
-
+    const {id,date}= req.body;
+    try{
+        const trans = await transactionSchema.find({userId:id,date:date})
+    }catch(err){
+        res.json({message:"No transactions on given date found"})
+    }
 }
 
 const getTransactionsByMonth=async(req,res)=>{
-
+    const {id,category}= req.body;
+    try{
+        const trans = await transactionSchema.find({userId:id,category:category})
+    }catch(err){
+        res.json({message:"No transactions by given month found"})
+    }
 }
 
 const getTransactionsByYear=async(req,res)=>{
-     
+    const {id,category}= req.body;
+    try{
+        const trans = await transactionSchema.find({userId:id,category:category})
+    }catch(err){
+        res.json({message:"No transactions by given year found"})
+    }
 }
 
 const getTotalStats=async(req,res)=>{
-   
+    const {id} = req.body;
+    try{
+        const income = await transactionSchema.find({userId:id,type:income})
+        const expense = await transactionSchema.find({userId:id,type:expense})
+        const balance = income-expense
+        res.json({income,expense,balance})
+
+
+    }catch(err){
+        res.json({message:"No stats found"})
+    }
 }
 
