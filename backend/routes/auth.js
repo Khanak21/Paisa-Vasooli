eb8870a00a9a587f7eb7ac973d52b7f2c42138c4
Sahil Gupta <gupsahil2005@gmail.com>
2023-11-06 18:56:34 +0530

    signup,signin,logout done and some routing done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {signup,signin} from '../controllers/auth.js'
+import {signup} from '../controllers/auth.js'
+import { signin } from "../controllers/signin.js";
+import {signout} from '../controllers/signout.js';
 const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/signin",signin);
+router.post("/signout",signout);
 
 export default router;
