08fd590730aa3f5d994824f88c0a537181c1e171
Khanak21 <khanakpatwari21@gmail.com>
2023-11-20 11:33:52 +0530

    Add friends to group feature done

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { creategroup,joingroup,getgroups,getmembers,splitBill, markPaid, markApproved,deleteGroup, simplifyDebt, approveDebt, getDebts,getgroup} from "../controllers/groups.js";
+import { creategroup,joingroup,getgroups,getmembers,splitBill, markPaid, markApproved,deleteGroup, simplifyDebt, approveDebt, getDebts,getgroup, addFriendsToGroup} from "../controllers/groups.js";
 
 const router = express.Router();
 router.post("/creategroup",creategroup)
 router.post("/joingroup",joingroup)
+router.put("/addfriendsgroup/:id",addFriendsToGroup)
 router.get("/getgroups/:id",getgroups)
 router.get("/getgroup/:id",getgroup)
 router.get("/getmembers/:id",getmembers)
