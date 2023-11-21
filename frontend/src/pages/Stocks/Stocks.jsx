f853ba6b60ebbee14e67f93d9d824c4f4de87663
Sahil Gupta <gupsahil2005@gmail.com>
2023-11-21 19:47:08 +0530

    added personalized stocks

--- frontend/src/pages/Stocks/Stocks.jsx
+++ frontend/src/pages/Stocks/Stocks.jsx
@@ -7,68 +7,73 @@ import StockChart from '../../components/StockChart'
 import HeatmapStocks from '../../components/HeatmapStocks'
 
 const Stocks = ({user,thememode,toggle}) => {
-  //   const [input,setInput]=useState({})
-  //   // const [data,setData] = useState([{ ticker: 'AAPL', company: 'Apple' }])
-  // const [stockData,setStockData]=useState(['AAPL'])
-  // console.log(stockData)
-  //   //handling user input for ticker symbol and company/crypto name
-  //   const handleInput = e=>{
-  //       setInput(prev=>([...prev,e.target.value]))
-  //       console.log(input)
-  //   }
-  //   const handleSubmit = async()=>{
-  //       try{
-  //           const res=await axios.post(`http://localhost:3001/api/user/addStock/${user._id}`,{input})
-  //           console.log(res.data.user.stocks)
-  //           const val=res.data.user.stocks
-  //           setStockData(prev=>([...prev,val]))
-  //           setInput("")
-
-  //       }catch(err){
-  //           console.log(err)
-  //       }
-  //   }
-  //   useEffect(()=>{
-  //     const getStocks = async()=>{ try{ const res=await axios.get(`http://localhost:3001/api/user/getStocks/${user._id}`)
-  //       console.log("widget",res.data)
-  //       setStockData(res.data.val)
-  //   }catch(err){
-  //       console.log(err)
-  //   }
-  //   }
-  //   getStocks()
-
-  //   },[])
-
+    const [input,setInput]=useState()
+    const[flag,setflag]=useState(false)
+    const[stockflag,setstockflag]=useState(false)
+    const[sym,setsym] = useState("MSFT")
+    // const [data,setData] = useState([{ ticker: 'AAPL', company: 'Apple' }])
+    const [stockData,setStockData]=useState(['AAPL'])
+    console.log(stockData)
+    //handling user input for ticker symbol and company/crypto name
+    const handleInput = e=>{
+        setInput(e.target.value)
+        console.log(input)
+    }
+    const handleSETSYM = async(e)=>{
+      setsym(e)
+      console.log("mausam",sym)
+      setstockflag(prev=>!prev)
+    }
+    const handleSubmit = async()=>{
+        try{
+            const res=await axios.post(`http://localhost:3001/api/user/addStock/${user._id}`,{input})
+            console.log(res.data.user.stocks)
+            const val=res.data.user.stocks
+            setStockData(prev=>([...prev,val]))
+            setflag(prev=>!prev)
+            setInput("")
+        }catch(err){
+            console.log(err)
+        }
+    }
+    useEffect(()=>{
+      const getStocks = async()=>{ try{ const res=await axios.get(`http://localhost:3001/api/user/getStocks/${user._id}`)
+        console.log("widget",res.data)
+        setStockData(res.data.val)
+        console.log(stockData)
+    }catch(err){
+        console.log(err)
+    }
+    }
+    getStocks()
+    console.log(stockData)
+    },[flag])
+    console.log(stockData)
 
   return ( 
     <div>
         <Navbar thememode={thememode} toggle={toggle}/>
-        {/* <div className="h-[60vh] border-2 mx-4 my-4 flex justify-center">
-        <div className='flex flex-col border-3 w-1/3 mx-2 my-2 justify-center'>
-            <input type='text' placeholder='Enter ticker symbol' className='my-2' name='ticker' onChange={handleInput('ticker')}></input>
-        <button className='bg-[#198754] my-4 rounded-md py-2' onClick={handleSubmit}>Add</button>
-        </div> */}
-
-
-        {/* <StockWidget user={user} stockData={stockData}/> */}
-        {/* {
-            stockData?.map((data)=>(
-             <StockChart data={data}/>
-            )
-            )
-            
-        }
-        helooooooooooooo */}
-        {/* </div> */}
         <div className="mx-auto my-auto h-[200vh] block justify-center items-center" style={{backgroundColor:thememode==="dark"?"#181818":"white"}} >
         <div className='flex w-full justify-center p-2 font-bold text-2xl' style={{color:thememode==="dark"?"white":"black"}}> Explore the Trendy Stocks...</div>
        <HeatmapStocks thememode={thememode}/>
-        
        <div className='flex w-full justify-center p-2 font-bold text-2xl' style={{color:thememode==="dark"?"white":"black"}}> Search for a particular stock/crypto...</div>
+       <div>
+       <label htmlFor='stocks'>Add Stocks: </label>
+       <input name={"input"}
+                   type="text"
+                   value={input}
+                   onChange={handleInput}
+                   required
+                   ></input>
+                   <button onClick={handleSubmit}>Save</button>
+      {stockData.map((stock, index) => (
+        <div key={index} onClick={()=>handleSETSYM(stock.input)}  style={{ cursor: "pointer", border: "1px solid black", padding: "5px" }}>
+          {stock.input}
+        </div>
+      ))}
+    </div>
         <div className='p-4'>
-        <TradingViewWidget sym={"AAPL"} thememode={thememode} />
-        {/* <TradingViewWidget sym={"MSFT"}/> */}
+        <TradingViewWidget sym={sym} stockflag={stockflag} thememode={thememode} />
          </div>
    
 
