515ef246e8b472e75031a9b96004fb81bf7b12ca
unknown <shikharpandya007@gmail.com>
2023-11-24 21:13:34 +0530

    some more changes

--- frontend/src/pages/inbox.jsx
+++ frontend/src/pages/inbox.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import Navbar from '../components/Navbar';
+import React, { useEffect, useState } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 import axios from "axios"
 
 const Inbox = ({ user,setUser,thememode,toggle }) => {
@@ -18,14 +18,33 @@ const Inbox = ({ user,setUser,thememode,toggle }) => {
         console.log(err)
     }
   }
-//   let arr=user.inbox
-
-
+const [inboxuser,setinboxuser] = useState({})
+useEffect(()=>{
+  const check=async()=>{
+    try{
+      const loggedInUser = localStorage.getItem("user");
+      if (loggedInUser) {
+        console.log(loggedInUser);
+        const foundUser = JSON.parse(loggedInUser);
+        console.log("found user",foundUser  )
+        setinboxuser(foundUser)
+        await setUser(foundUser);
+      }
+    }catch(err){
+      console.log(err)
+    }
+  }
+  check()
+},[user?._id])
+console.log(inboxuser)
   return (
+
     <div className='dark:bg-[#181818] h-[100vh]'>
       <Navbar thememode={thememode} toggle={toggle} />
+      <div className='font-extrabold text-5xl mx-4 mt-4 underline underline-offset-8 decoration-[#8656cd] dark:text-[#f0f0f0]'>Inbox</div>
+
       <div >
-      {user.inbox?.map((msg, index) => {
+      {inboxuser.inbox?.toReversed().map((msg, index) => {
         const tokens = msg.split(' ');
         const key = tokens[0];
 
@@ -34,7 +53,7 @@ const Inbox = ({ user,setUser,thememode,toggle }) => {
             {msg.includes('sent') ? (
               <div className='m-4 bg-gray-200 p-2 rounded-md flex  justify-between align-middle dark:bg-[#282828] dark:text-white'>
                 <div className='m-3'>{msg}</div>
-                <button className='p-2 m-2 bg-green-600 rounded-md text-white' onClick={()=>handleAccept(key)}>{user.friends.includes(key) ? "Accepted" : "Accept"}</button>
+                <button className='p-2 m-2 rounded-md text-white' style={{backgroundColor:inboxuser.friends.includes(key) ? "green":"#8656cd"}} onClick={()=>handleAccept(key)}>{inboxuser.friends.includes(key) ? "Accepted" : "Accept"}</button>
               </div>
             ) : (
               <div className='m-4 bg-gray-200 p-2 rounded-md dark:bg-[#282828] dark:text-white'>{msg}</div>
@@ -47,4 +66,4 @@ const Inbox = ({ user,setUser,thememode,toggle }) => {
   );
 };
 
-export default Inbox;
+export default Inb
