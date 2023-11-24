8850f6ebc66149167545291f74b6ce53cc45ebb2
unknown <shikharpandya007@gmail.com>
2023-11-24 21:23:46 +0530

    Some changes

--- frontend/src/pages/inbox.jsx
+++ frontend/src/pages/inbox.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../../components/Navbar/Navbar';
+import React, { useEffect,useState } from 'react';
+import Navbar from '../components/Navbar';
 import axios from "axios"
 
-const Inbox = ({ user,setUser,thememode,toggle }) => {
-  useEffect(() => {
-
-  }, [user.inbox,user.friends]);
+const Inbox = ({ user,setUser }) => {
+    const [inbox,setInbox] = useState(user.inbox)
 
   const handleAccept=async(key)=>{
     try{
@@ -14,56 +12,46 @@ const Inbox = ({ user,setUser,thememode,toggle }) => {
         alert(res.data.message)
         setUser(res.data.res1);
 
+
     }catch(err){
         console.log(err)
     }
   }
-const [inboxuser,setinboxuser] = useState({})
-useEffect(()=>{
-  const check=async()=>{
-    try{
-      const loggedInUser = localStorage.getItem("user");
-      if (loggedInUser) {
-        console.log(loggedInUser);
-        const foundUser = JSON.parse(loggedInUser);
-        console.log("found user",foundUser  )
-        setinboxuser(foundUser)
-        await setUser(foundUser);
-      }
-    }catch(err){
-      console.log(err)
-    }
-  }
-  check()
-},[user?._id])
-console.log(inboxuser)
+  useEffect(()=>{
+     const getinbox = async()=>{
+        try{
+            const res=await axios.get(`http://localhost:3001/api/user/getStocks/${user._id}`)
+            setInbox(res.data.inbox)
+        }catch(err){
+            console.log(err)
+        }
+     }
+  },[])
+
+
   return (
-
-    <div className='dark:bg-[#181818] h-[100vh]'>
-      <Navbar thememode={thememode} toggle={toggle} />
-      <div className='font-extrabold text-5xl mx-4 mt-4 underline underline-offset-8 decoration-[#8656cd] dark:text-[#f0f0f0]'>Inbox</div>
-
-      <div >
-      {inboxuser.inbox?.toReversed().map((msg, index) => {
+    <div>
+      <Navbar />
+      {inbox?.map((msg, index) => {
         const tokens = msg.split(' ');
         const key = tokens[0];
 
         return (
           <div key={key}>
             {msg.includes('sent') ? (
-              <div className='m-4 bg-gray-200 p-2 rounded-md flex  justify-between align-middle dark:bg-[#282828] dark:text-white'>
+              <div className='m-4 bg-gray-200 p-2 rounded-md flex  justify-between align-middle'>
                 <div className='m-3'>{msg}</div>
-                <button className='p-2 m-2 rounded-md text-white' style={{backgroundColor:inboxuser.friends.includes(key) ? "green":"#8656cd"}} onClick={()=>handleAccept(key)}>{inboxuser.friends.includes(key) ? "Accepted" : "Accept"}</button>
+                <button className='p-2 m-2 bg-green-600 rounded-md text-white' onClick={()=>handleAccept(key)}>{user.friends.includes(key) ? "Accepted" : "Accept"}</button>
               </div>
             ) : (
-              <div className='m-4 bg-gray-200 p-2 rounded-md dark:bg-[#282828] dark:text-white'>{msg}</div>
+              <div className='m-4 bg-gray-200 p-2 rounded-md'>{msg}</div>
             )}
           </div>
         );
       })}
     </div>
-    </div>
   );
 };
 
-export default Inb
+export default Inbox;
+
